1c013c7f6b60424ee5dd04091888156ded83d327
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-09-22 19:49:47 -0800

    Asteroids now spawn on the edge and move in a semi random direction across the screen

--- das-teroids/scripts/asteroid_spawner.gd
+++ das-teroids/scripts/asteroid_spawner.gd
@@ -6,42 +6,47 @@ var view: Rect2;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	view = get_viewport_rect();
+	
+func _process(_delta: float) -> void:
+	view = get_viewport_rect();
 
 # Spawn an asteroid on the edge of the viewport
 func _on_timer_timeout() -> void:
 	print("Spawning asteroid...")
 	var instance = ASTEROID.instantiate();
 	
-	##############################
-	#  Randomize spawn position  #
-	##############################
+	##########################################
+	#  Setup asteroid position and rotation  #
+	##########################################
 	
 	# Start in the top left corner of the view
 	var pos = Vector2(view.position.x, view.position.y);
-	var rot;
+	
+	# Add some offset to rotation
+	var rot = (randf() - 0.5);
+	
+	# Pixels past the edge of the screen asteroids will spawn
+	var screen_offset = 100;
+
 	match (randi() % 4):
 		0: # Top Edge
 			pos.x += randf() * view.size.x;
-			rot = 0;
+			pos.y -= screen_offset;
+			rot += 0;
 		1: # Bottom Edge
 			pos.x += randf() * view.size.x;
-			pos.y += view.size.y;
-			rot = PI;
+			pos.y += view.size.y + screen_offset;
+			rot += PI;
 		2: # Left Edge
 			pos.y += randf() * view.size.y;
-			rot = 3 * PI / 2;
+			pos.x -= screen_offset;
+			rot += 3 * PI / 2;
 		3: # Right Edge
 			pos.y += randf() * view.size.y;
-			pos.x += view.size.x;
-			rot = PI / 2;
+			pos.x += view.size.x + screen_offset;
+			rot += PI / 2;
 	
+	# Apply changes to instance	
 	instance.position = pos;
-	
-	#######################################
-	#  Point asteroid towards the screen  #
-	#######################################
-	
 	instance.set_rotation(rot);
-	
-	
 	add_child(instance);
