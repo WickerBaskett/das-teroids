c1ef3b7d6de739360389b0ace4bc94d264d59b2a
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-09-22 19:21:45 -0800

    Every asteroid now spawns facing away from the wall it spawns on

--- das-teroids/scripts/asteroid_spawner.gd
+++ das-teroids/scripts/asteroid_spawner.gd
@@ -7,25 +7,41 @@ var view: Rect2;
 func _ready() -> void:
 	view = get_viewport_rect();
 
+# Spawn an asteroid on the edge of the viewport
 func _on_timer_timeout() -> void:
 	print("Spawning asteroid...")
 	var instance = ASTEROID.instantiate();
 	
+	##############################
+	#  Randomize spawn position  #
+	##############################
+	
 	# Start in the top left corner of the view
 	var pos = Vector2(view.position.x, view.position.y);
+	var rot;
 	match (randi() % 4):
 		0: # Top Edge
 			pos.x += randf() * view.size.x;
+			rot = 0;
 		1: # Bottom Edge
 			pos.x += randf() * view.size.x;
 			pos.y += view.size.y;
+			rot = PI;
 		2: # Left Edge
 			pos.y += randf() * view.size.y;
+			rot = 3 * PI / 2;
 		3: # Right Edge
 			pos.y += randf() * view.size.y;
 			pos.x += view.size.x;
+			rot = PI / 2;
 	
 	instance.position = pos;
+	
+	#######################################
+	#  Point asteroid towards the screen  #
+	#######################################
+	
+	instance.set_rotation(rot);
 	
 	
 	add_child(instance);
