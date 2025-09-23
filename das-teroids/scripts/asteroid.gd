883b28752aba940b7e0dad35db33a0ee224ca3e8
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-09-22 18:31:12 -0800

    Added delta time to make asteroid speed independent of frame rate

--- das-teroids/scripts/asteroid.gd
+++ das-teroids/scripts/asteroid.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@export var speed: float = 5.0
+# @export allows us to modify this value in the editor
+@export var speed: float = 50.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +11,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.position.y += speed;
+	# Multiplying by delta makes asteroid speed consistent
+	# across different frame rates
+	self.position.y += speed * delta;
