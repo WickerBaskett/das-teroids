bcc426c8864a143931d1e7b7108214913e3db25b
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-09-25 23:17:18 -0800

    Implemented projectiles, they fire in the direction the player is facing

--- das-teroids/scripts/player.gd
+++ das-teroids/scripts/player.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const PROJECTILE = preload("uid://ddoufa6s84qes")
 
+var can_attack: bool = true
+
+# Called once per frame
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("fire") and can_attack:
+		fire()
 
 # This is the default movement function provided by CharacterBody2D
 # This will be changed with the Player Controller ticket
@@ -23,7 +30,39 @@ func _physics_process(_delta: float) -> void:
 
 	move_and_slide()
 
+# Handle spawning projectile
+func fire(): 
+	# Start attack cooldown
+	can_attack = false
+	get_node("AttackCooldown").start()
+
+	# Create projectile
+	var proj = PROJECTILE.instantiate()
+	get_parent().add_child(proj)
+
+	# The projectile is a child of the main_level scene
+	# so that it doesnt move with the player, we need to set
+	# the starting position to match the players!
+	proj.position = self.position
+	proj.rotation = self.rotation
+
+	# Get player dimensions so we can spawn the bullet outside
+	# of the player
+	var player_size = get_node("CollisionShape2D").get_shape().get_rect().size
+	var proj_size = proj.find_child("CollisionShape2D").get_shape().get_rect().size
+
+	# Offset bullet spawn to spawn outside of player
+	var rotated_pos = Vector2(
+	sin(self.rotation) * ((player_size.x / 2) + (proj_size.x / 2)), 
+	-cos(self.rotation) * ((player_size.y / 2) + (proj_size.y / 2))
+	)
+
+	proj.position += rotated_pos
 
 # Called on collision with asteroid
 func hit() -> void:
 	call_deferred("queue_free")
+
+# Called when player can attack again
+func _on_attack_cooldown_timeout() -> void:
+	can_attack = true
