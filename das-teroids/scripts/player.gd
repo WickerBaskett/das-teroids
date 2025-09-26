35ac38a8c60b4898e841da8a1702689063e22677
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-09-25 17:06:13 -0800

    Player now vanishes on collision with asteroid

--- das-teroids/scripts/player.gd
+++ das-teroids/scripts/player.gd
@@ -1,11 +1,27 @@
-extends Node2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
+# This is the default movement function provided by CharacterBody2D
+# This will be changed with the Player Controller ticket
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_direction := Input.get_axis("ui_left", "ui_right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var y_direction := Input.get_axis("ui_up", "ui_down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	move_and_slide()
+
+func hit() -> void:
+	self.hide();
