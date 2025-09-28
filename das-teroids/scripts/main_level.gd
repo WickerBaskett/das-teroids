c897176c92ccf911614929abd0b2697b40cdc51e
Elliott Lewandowski <elewandowski30@gmail.com>
2025-09-28 11:03:36 -0800

    Implemented game over logic

--- das-teroids/scripts/main_level.gd
+++ das-teroids/scripts/main_level.gd
@@ -1,8 +1,12 @@
 extends Node
 
 @onready var score_timer: Timer = $ScoreKeeper/ScoreTimer
+@onready var game_over: Label = $GameOver
+@onready var asteroid_spawner: Node2D = $AsteroidSpawner
 
 
 # Handles player death
 func _on_player_death() -> void:
+	asteroid_spawner.call_deferred("queue_free")
 	score_timer.stop()
+	game_over.visible = true
