8820ee59cd62d05f63ecf96fb230c534860f59a3
Alex Johnson <aej800@gmail.com>
2025-09-27 23:32:10 -0800

    changed damping on player velociities

--- das-teroids/scripts/player.gd
+++ das-teroids/scripts/player.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
+# Scales the linear speed of player
 const SPEED_SCALE = 10.0
+# Scales the rotation speed of player
 const ROTATION_SPEED_SCALE = 0.1
 
 
-# This is the default movement function provided by CharacterBody2D
-# This will be changed with the Player Controller ticket
 func _physics_process(_delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var player_rotation := Input.get_axis("ui_left", "ui_right")
 	var y_direction := Input.get_axis("ui_up", "ui_down")
 	
@@ -16,7 +14,6 @@ func _physics_process(_delta: float) -> void:
 	linear_velocity += global_transform.y * y_direction * SPEED_SCALE
 		
 	
-
 
 # Called on collision with asteroid
 func hit() -> void:
