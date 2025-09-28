74cf42b9eac7db74ebdb81e8c439bf4018422377
Alex Johnson <aej800@gmail.com>
2025-09-28 00:07:05 -0800

    ran gdformat on player.gd

--- das-teroids/scripts/player.gd
+++ das-teroids/scripts/player.gd
@@ -9,11 +9,10 @@ const ROTATION_SPEED_SCALE = 0.1
 func _physics_process(_delta: float) -> void:
 	var player_rotation := Input.get_axis("ui_left", "ui_right")
 	var y_direction := Input.get_axis("ui_up", "ui_down")
-	
+
 	angular_velocity += player_rotation * ROTATION_SPEED_SCALE
 	linear_velocity += global_transform.y * y_direction * SPEED_SCALE
-		
-	
+
 
 # Called on collision with asteroid
 func hit() -> void:
