61b0dd6b1114ff389ea372c7da8455cb1c1a5b24
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-10-01 12:09:14 -0800

    Linted and Formatted

--- das-teroids/scripts/menu.gd
+++ das-teroids/scripts/menu.gd
@@ -4,13 +4,16 @@ const MAIN_LEVEL = preload("uid://dhm28ne3u5nwa")
 
 @onready var box_cont: BoxContainer = $BoxContainer
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_viewport().connect("size_changed", _on_viewport_size_changed)
 
+
 #####################
 #  Signal Handlers  #
 #####################
+
 
 func _on_viewport_size_changed() -> void:
 	var view_size = get_viewport_rect().size
