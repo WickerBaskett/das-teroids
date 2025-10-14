5ed3baeb8958c86b144ed426b1a20d08efd292f3
Elliott Lewandowski <elewandowski30@gmail.com>
2025-10-13 18:14:57 -0800

    Das blast has a visual effect

--- das-teroids/scripts/power_up/das_blast.gd
+++ das-teroids/scripts/power_up/das_blast.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-const PROJECTILE = preload("uid://ddoufa6s84qes")
 @onready var sprite_2d: Sprite2D = %Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
 @onready var effect: Area2D = %Effect
+@onready var effect_sprite: Sprite2D = %EffectSprite
 @export var duration: float = 2.0
 var active: bool = false
 
@@ -11,6 +11,7 @@ func _on_collectable_collected(_player: RigidBody2D) -> void:
 	
 	sprite_2d.visible = false
 	collision_shape_2d.disabled = false
+	effect_sprite.visible = true
 	active = true
 	
 	await get_tree().create_timer(duration).timeout
@@ -22,5 +23,5 @@ func _physics_process(delta: float) -> void:
 		for area in effect.get_overlapping_areas():
 			if area.has_method("hit") and not area.is_in_group("Player"):
 				area.hit()
-
+		effect_sprite.scale += Vector2(0.255, 0.255)
 		collision_shape_2d.shape.set_deferred("radius", collision_shape_2d.shape.radius + 500 * delta)
