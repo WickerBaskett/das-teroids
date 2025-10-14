1b1a1a8384a652299f05db74aaa74e3b823e23b2
Alex Johnson <aej800@gmail.com>
2025-10-13 16:36:25 -0800

    formated moving.gd

--- das-teroids/scripts/player/states/moving.gd
+++ das-teroids/scripts/player/states/moving.gd
@@ -7,6 +7,7 @@ const ROTATION_SPEED_SCALE = 0.1  # Scales the rotation speed of player
 @onready var attack_cooldown: Timer = %AttackCooldown
 @onready var thruster_particles: GPUParticles2D = %"Thruster Particles"
 
+
 # Called from physics_process(delta: float)
 func physics_update(_delta: float) -> void:
 	var player_rotation := Input.get_axis("ui_left", "ui_right")
@@ -17,7 +18,7 @@ func physics_update(_delta: float) -> void:
 		emit_signal("transition", self, "idle")
 	elif y_direction < 1:
 		thruster_particles.emitting = true
-		
+
 	player.angular_velocity += player_rotation * ROTATION_SPEED_SCALE
 	player.linear_velocity += player.global_transform.y * y_direction * SPEED_SCALE
 
@@ -32,4 +33,3 @@ func process_update(_delta: float) -> void:
 
 	if Input.is_action_pressed("fire") and attack_cooldown.time_left == 0.0 and player.mag > 0:
 		emit_signal("transition", self, "shooting")
-		
