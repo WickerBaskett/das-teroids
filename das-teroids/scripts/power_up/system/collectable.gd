e3bf89fd13c067ca01642040e5bec46f8f3ec1e1
Elliott Lewandowski <elewandowski30@gmail.com>
2025-10-16 15:08:49 -0800

    Projectiles can now collect powerups

--- das-teroids/scripts/power_up/system/collectable.gd
+++ das-teroids/scripts/power_up/system/collectable.gd
@@ -4,9 +4,21 @@ signal collected(player: RigidBody2D)
 
 var collected_flag: bool = false
 
+func collect() -> void:
+	collected_flag = true
+	var bodies = get_tree().get_nodes_in_group("Players")
+	
+	if len(bodies) > 0:
+		emit_signal("collected", bodies[0])
+		SignalBus.emit_signal("stop_moving", self)
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players") and not collected_flag:
-		collected_flag = true
-		
-		emit_signal("collected", body)
-		SignalBus.emit_signal("stop_moving", self)
+		collect()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Projectile") and not collected_flag:
+		collect()
+		if area.has_method("hit"):
+			area.hit()
