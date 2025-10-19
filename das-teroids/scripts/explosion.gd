fdc4b200a33fd84e7ff7284d46198d4a555b0235
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-10-19 15:17:26 -0800

    Created explosion scene

---
+++ das-teroids/scripts/explosion.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var lifetime: Timer = %Lifetime
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await lifetime.timeout
+	call_deferred("queue_free")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
