1be2a175c6eea30f4d0275e0af634844b2aeb7f9
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-10-19 14:41:35 -0800

    removed powerups left in from debugging and fixed bug with schwartz shot destroying it's own projectiles

--- das-teroids/scripts/projectile.gd
+++ das-teroids/scripts/projectile.gd
@@ -23,6 +23,6 @@ func hit() -> void:
 # On collision with asteroid
 func _on_area_entered(area: Area2D) -> void:
 	print("Proj hit area...")
-	if area.has_method("hit"):
+	if area.has_method("hit") and !area.is_in_group("Projectile"):
 		area.hit()
 		call_deferred("queue_free")
